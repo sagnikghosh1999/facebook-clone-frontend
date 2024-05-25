4b553c251b7166c986278498a3d0a7c8cc78bd5a
sagnikghosh1999 <saggywebd@gmail.com>
2024-05-25 20:55:35 +0530

    search functionality

--- src/components/header/SearchMenu.jsx
+++ src/components/header/SearchMenu.jsx
@@ -1,9 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import useClickOutside from "../../helpers/clickOutside";
 import { Return, Search } from "../../svg";
+import {
+  addToSearchHistory,
+  getSearchHistory,
+  removeFromSearch,
+  search,
+} from "../../functions/user";
+import { Link } from "react-router-dom";
 
-export default function SearchMenu({ color, setShowSearchMenu }) {
+export default function SearchMenu({ color, setShowSearchMenu, token }) {
   const [iconVisible, setIconVisible] = useState(true);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [searchHistory, setSearchHistory] = useState([]);
 
   const menu = useRef(null);
   const input = useRef(null);
@@ -13,8 +24,35 @@ export default function SearchMenu({ color, setShowSearchMenu }) {
   });
 
   useEffect(() => {
+    getHistory();
+  }, []);
+  const getHistory = async () => {
+    const res = await getSearchHistory(token);
+    setSearchHistory(res);
+  };
+
+  useEffect(() => {
     input.current.focus();
   }, []);
+
+  const searchHandler = async () => {
+    if (searchTerm === "") {
+      setResults("");
+    } else {
+      const res = await search(searchTerm, token);
+      setResults(res);
+    }
+  };
+
+  const addToSearchHistoryHandler = async (searchUser) => {
+    await addToSearchHistory(searchUser, token);
+    getHistory();
+  };
+
+  const handleRemove = async (searchUser) => {
+    removeFromSearch(searchUser, token);
+    getHistory();
+  };
 
   return (
     <div className="header_left search_area scrollbar" ref={menu}>
@@ -41,11 +79,18 @@ export default function SearchMenu({ color, setShowSearchMenu }) {
             </div>
           )}
           <input
-            type={"text"}
+            type="text"
             placeholder="Search Facebook"
             ref={input}
-            onFocus={() => setIconVisible(false)}
-            onBlur={() => setIconVisible(true)}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyUp={searchHandler}
+            onFocus={() => {
+              setIconVisible(false);
+            }}
+            onBlur={() => {
+              setIconVisible(true);
+            }}
           />
         </div>
       </div>
@@ -53,8 +98,50 @@ export default function SearchMenu({ color, setShowSearchMenu }) {
         <span>Recent searches</span>
         <a href="/">Edit</a>
       </div>
-      <div className="search_history"></div>
-      <div className="search_results scrollbar"></div>
+      <div className="search_history">
+        {searchHistory &&
+          results == "" &&
+          searchHistory
+            .sort((a, b) => {
+              return new Date(b.createdAt) - new Date(a.createdAt);
+            })
+            .map((user) => (
+              <div className="search_user_item hover1" key={user._id}>
+                <Link
+                  className="flex"
+                  to={`/profile/${user.user.username}`}
+                  onClick={() => addToSearchHistoryHandler(user.user._id)}
+                >
+                  <img src={user.user.picture} alt="" />
+                  <span>
+                    {user.user.first_name} {user.user.last_name}
+                  </span>
+                </Link>
+                <i
+                  className="exit_icon"
+                  onClick={() => {
+                    handleRemove(user.user._id);
+                  }}
+                ></i>
+              </div>
+            ))}
+      </div>
+      <div className="search_results scrollbar">
+        {results &&
+          results.map((user) => (
+            <Link
+              to={`/profile/${user.username}`}
+              className="search_user_item hover1"
+              onClick={() => addToSearchHistoryHandler(user._id)}
+              key={user._id}
+            >
+              <img src={user.picture} alt="" />
+              <span>
+                {user.first_name} {user.last_name}
+              </span>
+            </Link>
+          ))}
+      </div>
     </div>
   );
 }
